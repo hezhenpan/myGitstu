1381d8fa67d02d1d14cdc48d1b698020ab05bc02
hezhenpan <hezhenpan@hotmail.com>
2019-08-17 21:38:17 +0800

    add (#5)
    * add mydb file
    
    * add 1 line of dbsql
    
    * add 2 line of db.sql
    
    * mod all the files
    
    * add real sql file

--- mydb/db.sql
+++ mydb/db.sql
@@ -1,4 +1,15 @@
-this is the first line
-this is the second line
-add the 3 line
-thrd mde 4 line
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  `class` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `teacher` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `degree` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
