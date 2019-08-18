38dbf7ce761ef7390af7714313a999df74cd6d4f
hezhenpan <hezhenpan@163.com>
2019-08-18 10:41:51 +0800

    add db sql

--- mydb/db.sql
+++ mydb/db.sql
@@ -13,3 +13,11 @@ CREATE TABLE `teacher` (
   `degree` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `school` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sname` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
